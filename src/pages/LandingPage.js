12cde29b8b412d7eb8a1c9c869a9712501f5d1e6
Hwangsooneun <catalyst8849@gmail.com>
2021-07-04 19:07:28 +0900

    test

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -38,7 +38,7 @@ export default function LandingPage() {
 
   return (
     <StyledLandingPage>
-      <CategorySelectModal />
+      {/* <CategorySelectModal /> */}
       <StyledSectionTop>
         <animated.div style={divProps}>
           <h3>목표 달성을 위한 긴 여정,</h3>
